2b565e2e29d9930e4156c3b5440df803de4e972c
PolinaSavel <limonka2005@mail.ru>
2024-12-02 12:32:29 +0500

    new branch hw2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,9 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        
+
+        // Первый урок изучение GIT, Solid, ООП
+
         System.out.println("Hello world!");
     }
 }
